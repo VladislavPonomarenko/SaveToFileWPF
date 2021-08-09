866a4378fcf3d7d2b8f5f926e10e0d3f41db3461
Vlad Ponomarenko <vladponomarenko8@gmail.com>
2021-08-09 19:08:19 +0300

    Add Caliburn.Micro(framework) and Bootstrapper class

--- SaveToFileWPF/App.xaml.cs
+++ SaveToFileWPF/App.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using SaveToFileWPF.ApplicationContext;
 using System.Windows;
 
 namespace SaveToFileWPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private Bootstrapper _bootstrapper;
+
+        public App()
+        {
+            _bootstrapper = new Bootstrapper();
+        }
     }
 }
---
+++ SaveToFileWPF/ApplicationContext/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using SaveToFileWPF.Screens.MainWindow;
+
+namespace SaveToFileWPF.ApplicationContext
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainWindowViewModel>();
+        }
+    }
+}
